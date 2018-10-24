82a1f71896e473d478a4bbaaeef75c95ffc4f67e
Jamie McGrory <contact@jmcgr.co.uk>
2018-10-24 21:52:34 +0100

    adding routes

--- src/app/Components/Layout/navigation/navigation.component.ts
+++ src/app/Components/Layout/navigation/navigation.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
     <ul>
       <li><a>Home</a></li>
       <li><a>Bikes</a></li>
-      <li><a>Compare</a></li>
       <li><a>Contact</a></li>
     </ul>
   `,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,17 +10,46 @@ import { FooterComponent } from './Components/Layout/footer/footer.component';
 import { NavigationComponent } from './Components/Layout/navigation/navigation.component';
 import { TableComponent } from './Components/Layout/table/table.component';
 
+import { HomeComponent } from './Components/Route/home/home.component';
+import { ContactComponent } from './Components/Route/contact/contact.component';
+import { BikesComponent } from './Components/Route/bikes/bikes.component';
+import { LostComponent } from './Components/Route/lost/lost.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'bikes',
+    component: BikesComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: '**',
+    component: LostComponent
+  }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     NavigationComponent,
-    TableComponent
+    TableComponent,
+    HomeComponent,
+    ContactComponent,
+    BikesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
