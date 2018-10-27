598ce55396791198f367021897a4ef0d9eaac3b9
Jamie McGrory <contact@jmcgr.co.uk>
2018-10-27 15:38:43 +0100

    weight property added

---
+++ src/app/models/properties/numerical/WeightProperty.ts
@@ -0,0 +1,19 @@
+import NumericalProperty from './NumericalProperty';
+
+class WeightProperty extends NumericalProperty {
+
+    public units = 'kg';
+
+    public static availableUnits = new Map([
+
+        ['g', (value) => value],
+
+        ['kg', (value) => (value / 1000)],
+
+        ['lbs', (value) => (value / 0.00220462)],
+
+    ]);
+
+}
+
+export default WeightProperty;
