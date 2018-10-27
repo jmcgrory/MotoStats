12e958aaad9f14c2e9349992a90e941e41b6fb59
Jamie McGrory <contact@jmcgr.co.uk>
2018-10-27 12:54:01 +0100

    adding basic types

---
+++ src/app/models/Types.ts
@@ -0,0 +1,15 @@
+type BrakeType = 'single disc' | 'twin disc' | 'drum' | 'none';
+
+type FinalDrive = 'chain' | 'belt' | 'shaft';
+
+type FuelType = 'four stroke' | 'two stroke';
+
+export {
+
+    BrakeType,
+
+    FinalDrive,
+
+    FuelType,
+
+}
