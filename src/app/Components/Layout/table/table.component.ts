78f2d59f0f5d7811c0551a5e29bbd3937d44367b
Jamie McGrory <contact@jmcgr.co.uk>
2018-10-27 14:38:03 +0100

    scss var amends + font styling changes

--- src/app/Components/Layout/table/table.component.ts
+++ src/app/Components/Layout/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import BikeModel from '../../../models/BikeModel';
 
 @Component({
   selector: 'table-component',
@@ -6,6 +7,102 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+
+  columns: object[] = [
+
+    { name: 'id', key: 'id', options: false },
+
+    { name: 'manufacturer', key: 'manufacturer', options: false },
+
+    { name: 'model', key: 'model', options: false },
+
+    { name: 'cc', key: 'displacement', options: false },
+
+    { name: 'year', key: 'year', options: false },
+
+    { name: 'ABS', key: 'hasABS', options: false },
+
+  ];
+
+  getRandomFromArray = (array: string[]): string => {
+
+    return array[Math.floor(Math.random() * array.length)];
+
+  }
+
+  manufacturers = [
+
+    'Honda',
+
+    'Triumph',
+
+    'Ducati',
+
+    'Kawasaki'
+
+  ];
+
+  models = [
+
+    'Viper',
+
+    'FST',
+
+    'Supercruiser',
+
+    'Speciale',
+
+    'XTR',
+
+    'Hawk',
+
+    'Scrambler',
+
+    'V5',
+
+    'Baja'
+
+  ];
+
+  private getPlaceholderData = (): object => {
+
+    return {
+
+      id: Math.floor(Math.random() * 100),
+
+      manufacturer: this.getRandomFromArray(this.manufacturers),
+
+      displacement: Math.floor(Math.random() * 1600),
+
+      year: 1988 + (Math.floor(Math.random() * 30)),
+
+      hasABS: (Math.round(Math.random()) ? 'Yes' : 'No'),
+
+      model: this.getRandomFromArray(this.models)
+
+    }
+
+  }
+
+  rows: BikeModel[] = [
+
+    new BikeModel(this.getPlaceholderData()),
+
+    new BikeModel(this.getPlaceholderData()),
+
+    new BikeModel(this.getPlaceholderData()),
+
+    new BikeModel(this.getPlaceholderData()),
+
+    new BikeModel(this.getPlaceholderData()),
+
+    new BikeModel(this.getPlaceholderData()),
+
+    new BikeModel(this.getPlaceholderData()),
+
+    new BikeModel(this.getPlaceholderData())
+
+  ];
 
   constructor() { }
 
