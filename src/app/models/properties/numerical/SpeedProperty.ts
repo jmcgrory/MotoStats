7a7cb23139a7f9f3fe6e1da91a5a10393c38fb24
Jamie McGrory <contact@jmcgr.co.uk>
2018-10-27 15:26:08 +0100

    speed property

---
+++ src/app/models/properties/numerical/SpeedProperty.ts
@@ -0,0 +1,17 @@
+import NumericalProperty from './NumericalProperty';
+
+class SpeedProperty extends NumericalProperty {
+
+    public units = 'kph';
+
+    public static availableUnits = new Map([
+
+        ['kph', (value) => value],
+
+        ['mph', (value) => (value / 0.621371)]
+
+    ]);
+
+}
+
+export default SpeedProperty;
